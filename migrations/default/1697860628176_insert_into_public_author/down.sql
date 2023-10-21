f9b2a6eacd93934d8f84b20135606fdd95b97055
M Danil Rafiqi <mdanilrafiqi@Ms-Mac-mini.local>
2023-10-21 11:14:12 +0700

    feat: insert

---
+++ migrations/default/1697860628176_insert_into_public_author/down.sql
@@ -0,0 +1,1 @@
+DELETE FROM "public"."author" WHERE "id" = '9bf093e2-4629-4e72-a4bd-b80c6816a0bf';
---
+++ migrations/default/1697860628176_insert_into_public_author/up.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."author"("rating", "name", "id") VALUES (5, E'daniltest', E'9bf093e2-4629-4e72-a4bd-b80c6816a0bf');
