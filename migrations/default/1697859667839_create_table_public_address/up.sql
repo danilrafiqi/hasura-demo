a66c09e2322c2f09ac5be42648f1bae65708194d
M Danil Rafiqi <mdanilrafiqi@Ms-Mac-mini.local>
2023-10-21 10:51:58 +0700

    feat: add address

---
+++ migrations/default/1697859667839_create_table_public_address/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."address" ("id" uuid NOT NULL, "street" text NOT NULL, "zip" text NOT NULL, "city" text NOT NULL, "country" text NOT NULL, "author_id" uuid NOT NULL, PRIMARY KEY ("id") );
